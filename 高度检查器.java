b5538f0e4e0f264b3236040fbebca45483469b31
CITRITE\changsongz <Changsong.zhou@citrix.com>
2022-06-17 16:47:42 +0800

    lean

---
+++ 高度检查器.java
@@ -0,0 +1,23 @@
+//桶排序
+class Solution {
+    public int heightChecker(int[] heights) {
+        // 值的范围是1 <= heights[i] <= 100，因此需要1,2,3,...,99,100，共101个桶
+        int[] arr = new int[101];
+        // 遍历数组heights，计算每个桶中有多少个元素，也就是数组heights中有多少个1，多少个2，。。。，多少个100
+        // 将这101个桶中的元素，一个一个桶地取出来，元素就是有序的
+        for (int height : heights) {
+            arr[height]++;
+        }
+ 
+        int count = 0;
+        for (int i = 1, j = 0; i < arr.length; i++) {
+            // arr[i]，i就是桶中存放的元素的值，arr[i]是元素的个数
+            // arr[i]-- 就是每次取出一个，一直取到没有元素，成为空桶
+            while (arr[i]-- > 0) {
+                // 从桶中取出元素时，元素的排列顺序就是非递减的，然后与heights中的元素比较，如果不同，计算器就加1
+                if (heights[j++] != i) count++;
+            }
+        }
+        return count;
+     }
+ }
